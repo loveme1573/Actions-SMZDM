57e284194c2c7a652efad5a79bcd93368dcdca01
loveme1573 <76917552+loveme1573@users.noreply.github.com>
2022-09-10 18:49:29 +0800

    Update smzdm.js

--- function/smzdm.js
+++ function/smzdm.js
@@ -14,13 +14,17 @@ const PASS = process.env.SMZDM_PASS
 const SEND_KEY = process.env.SEND_KEY
 const UTC8 = new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000;
 
+*/
 async function downFile () {
     const url1 = 'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/smzdm/backup/smzdm_checkin.js'
+    const url12 = 'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/smzdm/smzdm_mission.js'
     // const url1 = 'https://raw.githubusercontent.com/jiegto/Actions_smzdm/main/backup/smzdm_checkin.js'
     const url2 = 'https://raw.githubusercontent.com/loveme1573/Actions-SMZDM/main/function/magic.json'    
     await download(url1, './')
+    await download(url12, './')
     await download(url2, './')
 }
+*/
 
 async function changeFiele () {
     let content = await fs.readFileSync('./magic.json', 'utf8')
